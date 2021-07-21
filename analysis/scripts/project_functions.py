23696f5ba2a4578e87be449c11a7ebf0a28fd4be
Bilel Matmti <bilel.matmti@brentwood.ca>
2021-07-22 00:38:04 +0300

    fixing relative import

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -7,8 +7,7 @@ def load_and_process(url_or_path_to_csv_file):
 
     df = (   
          pd.read_csv(url_or_path_to_csv_file) 
-        .rename(columns={"bmi": "bmi (Body Mass Index)"})
-        .rename(columns={"region": "USA Region"})
+        .rename(columns={"bmi": "bmi (Body Mass Index)", "region": "USA Region"})
         .loc[lambda x: x['age']>17]
         .sort_values("age", ascending=True)    
         )
